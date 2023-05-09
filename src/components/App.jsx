30c98e0b971a9137744ce747c33c52dd7a5c0796
Djoult <55453354+Djoult@users.noreply.github.com>
2023-05-09 13:30:00 +0300

    delete test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,6 @@
 import { Component } from 'react';
-// import ContactForm from './ContactForm/ContactForm';
-// import Contacts from './Contacts/Contacts';
-import Profile from './Profile/Profile';
-import user from './Profile/user.json';
+import ContactForm from './ContactForm/ContactForm';
+import Contacts from './Contacts/Contacts';
 
 export class App extends Component {
   state = {
@@ -12,15 +10,8 @@ export class App extends Component {
   render() {
     return (
       <>
-        {/* <ContactForm />
-        <Contacts contacts={this.state.contacts} /> */}
-        <Profile
-          username={user.username}
-          tag={user.tag}
-          location={user.location}
-          avatar={user.avatar}
-          stats={user.stats}
-        />
+        <ContactForm />
+        <Contacts contacts={this.state.contacts} />
       </>
     );
   }
